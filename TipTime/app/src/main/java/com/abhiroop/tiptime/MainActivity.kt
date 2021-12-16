3da163298a89f48181468248d3ba48303c2f4a8a
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-12-17 00:23:36 +0530

    [NEW] added program functionality to the app

--- TipTime/app/src/main/java/com/abhiroop/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/abhiroop/tiptime/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.abhiroop.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.abhiroop.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        // If the cost is null or 0, then display 0 tip and exit this function early.
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            binding.optionTwentyPercent.id -> 0.20
+            binding.optionEighteenPercent.id -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+
+        if (binding.roundUpSwitch.isChecked)
+            tip = kotlin.math.ceil(tip)
+
+        // Display the formatted tip value on screen
+        displayTip(tip)
     }
 }
