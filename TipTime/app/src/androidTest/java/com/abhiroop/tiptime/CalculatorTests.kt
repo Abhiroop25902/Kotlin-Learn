e4d8dbb5061be15f8ec02fa04a11277479c98a61
Abhiroop25902 <69428539+Abhiroop25902@users.noreply.github.com>
2021-12-20 18:14:57 +0530

    [NEW] material guidelines in Tip Time App

---
+++ TipTime/app/src/androidTest/java/com/abhiroop/tiptime/CalculatorTests.kt
@@ -0,0 +1,63 @@
+package com.abhiroop.tiptime
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import org.hamcrest.CoreMatchers.containsString
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class CalculatorTests {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
+    }
+
+    @Test
+    fun calculate_18_percent_tip(){
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("100.00"))
+
+        onView(withId(R.id.option_eighteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$18.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip(){
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("100.00"))
+
+        onView(withId(R.id.option_fifteen_percent))
+            .perform(click())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$15.00"))))
+    }
+}
--- TipTime/app/src/main/java/com/abhiroop/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/abhiroop/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.abhiroop.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.abhiroop.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -15,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun displayTip(tip : Double) {
@@ -23,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
         // If the cost is null or 0, then display 0 tip and exit this function early.
         if (cost == null || cost == 0.0) {
             displayTip(0.0)
@@ -42,4 +48,16 @@ class MainActivity : AppCompatActivity() {
         // Display the formatted tip value on screen
         displayTip(tip)
     }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
 }
